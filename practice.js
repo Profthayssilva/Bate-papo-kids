341429a5c60befd8fb7ae6459230e0d126a1c882
Thays Silva <113531216+Profthayssilva@users.noreply.github.com>
2023-05-10 20:06:39 -0300

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,15 +1,13 @@
-
-// ADICIONE SUS LINKS FIREBASE AQUI
 var firebaseConfig = {
-  apiKey: "AIzaSyCYXiTh85QfTzkY0hkYZYjhODf8lVbtKgU",
-  authDomain: "kwitterprofthays.firebaseapp.com",
-  databaseURL: "https://kwitterprofthays-default-rtdb.firebaseio.com",
-  projectId: "kwitterprofthays",
-  storageBucket: "kwitterprofthays.appspot.com",
-  messagingSenderId: "182007721289",
-  appId: "1:182007721289:web:f20b13c099fe4e9a507e98"
-};
-
+  apiKey: "AIzaSyC3b3zbcR7dUSfmI-snzX5xKclbpa5fl80",
+  authDomain: "eassimquefazmarkting.firebaseapp.com",
+  databaseURL: "https://eassimquefazmarkting-default-rtdb.firebaseio.com",
+  projectId: "eassimquefazmarkting",
+ storageBucket: "eassimquefazmarkting.appspot.com",
+  messagingSenderId: "722971396077",
+  appId: "1:722971396077:web:8d79a7409e12288137c9e4"
+  };
+  
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
